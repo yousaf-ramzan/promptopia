78d29784582fedacf5d3781edaf4e0c5dfe2e91e
yousaf Ramzan <yousaf.ramzan@devprotocols.com>
2023-07-11 15:07:39 +0500

    add data to home page

--- app/page.js
+++ app/page.js
@@ -1,5 +1,16 @@
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <section className="w-full flex-center flex-col">
+      <h1 className="head_text text-center">Discover & share</h1>
+      <br className="max-md:hidden" />
+      <span className="orange_gradient text-center">AI-Powered Prompts</span>
+      <p className="desc text-center">
+        {" "}
+        Promptpia is an open-source AI prompting tool for modren world to
+        discover, create and share creative prompts{" "}
+      </p>
+    </section>
+  );
 };
 
 export default Home;
